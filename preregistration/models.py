8d1c69d3eef67215076a107283de7cd4e8d4e6df
sanchit-ahuja <sanchitahuja205@gmail.com>
2018-04-18 13:05:14 +0530

    Models for roctaves

--- preregistration/models.py
+++ preregistration/models.py
@@ -4,3 +4,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Roctaves(models.Model):
+    name = models.CharField(max_length=100)
+	city = models.CharField(max_length=100)
+	phone = models.CharField(default='' , blank = False, max_length=13)
+	gender = models.CharField(max_length=6)
+	email_address = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+        
