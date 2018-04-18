8a9c1a7657c2bcba7267f20ef7dd1793114f8262
sanchit-ahuja <sanchitahuja205@gmail.com>
2018-04-18 13:14:02 +0530

    d

--- preregistration/models.py
+++ preregistration/models.py
@@ -6,12 +6,11 @@ from django.db import models
 # Create your models here.
 
 class Roctaves(models.Model):
-    name = models.CharField(max_length=100)
+	name = models.CharField(max_length=100)
 	city = models.CharField(max_length=100)
 	phone = models.CharField(default='' , blank = False, max_length=13)
 	gender = models.CharField(max_length=6)
 	email_address = models.EmailField(unique=True)
-
-    def __str__(self):
-        return self.name
+	def __str__(self):
+		return self.name
         
