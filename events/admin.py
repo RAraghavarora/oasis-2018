27ccc5877ac48c3edba731fa206bace93dba8de0
Nayan-Khanna <f20170636@pilani.bits-pilani.ac.in>
2018-04-20 16:51:25 +0530

    Updated

--- events/admin.py
+++ events/admin.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import *
+from .models import Category, Participation, IntroEvent
 
 admin.site.register(Category)
 admin.site.register(Participation)
