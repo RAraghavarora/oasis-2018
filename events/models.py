41f53f6fe881d4641f82d198b8f98f7cbb7d781e
Hemanth V. Alluri <hdrive1999@gmail.com>
2018-05-18 18:17:37 +0530

    Update: Cleared up the functionality issue due to miscommunication and also made the excel sheet better to look at.

--- events/models.py
+++ events/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
+from django.contrib.auth.models import User
 from preregistration.models import GenParticipant
 
 class Category(models.Model):
@@ -14,17 +15,18 @@ class Category(models.Model):
 
 
 class IntroEvent(models.Model):
-	name = models.CharField(max_length=100,unique=True)
-	short_description = models.CharField(blank=True,max_length=140)
-	rules = models.CharField(blank=True,max_length=200)
-	category = models.ForeignKey('Category', default=3)
-	contact = models.CharField(max_length=140, default='')
+    user = models.OneToOneField(User, related_name="eventmodel", null=True)
+    name = models.CharField(max_length=100,unique=True)
+    short_description = models.CharField(blank=True,max_length=140)
+    rules = models.CharField(blank=True,max_length=200)
+    category = models.ForeignKey('Category', default=3)
+    contact = models.CharField(max_length=140, default='')
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
 class Participation(models.Model):
-	event = models.ForeignKey(IntroEvent, on_delete=models.CASCADE)
+	event = models.ForeignKey(IntroEvent, related_name="participation" ,on_delete=models.CASCADE)
 	participant = models.ForeignKey('preregistration.GenParticipant', on_delete=models.CASCADE, null=True)
 	pcr_approved = models.BooleanField(default=False)
 	cr_approved = models.BooleanField(default=False)
--- events/views.py
+++ events/views.py
@@ -11,9 +11,10 @@ from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from django.core.exceptions import *
 
-from .models import *
-
+from events.models import *
+from preregistration.models import *
 
 @csrf_exempt
 def IntroEventsData(request):
@@ -22,9 +23,9 @@ def IntroEventsData(request):
         return render(request, "events/IntroEventsData.html")
 
     if request.method == "POST":
-        # Authentication Step:
+
         try:
-            # receive the username and password accordingly
+            # STEP 1: receive the username and password from the request based on content_type
             content_type = request.META["CONTENT_TYPE"].split(";")[0]
             if content_type == "application/x-www-form-urlencoded" or content_type == "multipart/form-data":
                 username = request.POST["username"]
@@ -36,39 +37,49 @@ def IntroEventsData(request):
             else:
                 raise ContentTypeError()
 
-            # check database for user and act accordingly
-            user = authenticate(username=username, password=password)
-            if not user:
+            # STEP 2: check database for user and is an event user
+            event_user = authenticate(username=username, password=password)
+            if not event_user:
                 raise InvalidCredentialsError()
-            if not user.is_staff:
+            try: # check to see if the user is associated with an event
+                event = event_user.eventmodel
+            except Exception:
                 raise InvalidPermissionsError()
 
-            # Excel Sheet Generation Step after Authentication
-            wb = Workbook()
-            ws = wb.active
-            ws.title = "Events Data"
+            # STEP 3: Excel Sheet Generation Step after Authentication
+            databook = Workbook()
+            datasheet = databook.active
+            datasheet.title = "{} Data".format(username)
 
-            # set title row
-            ws["A1"] = "Name"
-            ws["B1"] = "Short Description"
-            """increase the column width -> pending"""
-            ws["C1"] = "Rules"
-            ws["D1"] = "Category"
-            ws["E1"] = "Contact"
+            datasheet["A1"] = "Name"
+            datasheet.column_dimensions["A"].width = 20
+            datasheet["B1"] = "PCR Approval"
+            datasheet.column_dimensions["B"].width = 15
+            datasheet["C1"] = "CR Approval"
+            datasheet.column_dimensions["C"].width = 15
+            datasheet["D1"] = "Email Address"
+            datasheet.column_dimensions["D"].width = 30
+            datasheet["E1"] = "Phone Number"
+            datasheet.column_dimensions["E"].width = 15
 
-            # now fill Data
-            counter = 1
-            for event in IntroEvent.objects.all():
+            counter = 2
+            for participation in event.participation.all():
+                datasheet["A{}".format(counter)] = participation.participant.name
+                if participation.pcr_approved:
+                    datasheet["B{}".format(counter)] = "Approved"
+                else:
+                    datasheet["B{}".format(counter)] = "Not Approved"
+                if participation.cr_approved:
+                    datasheet["C{}".format(counter)] = "Approved"
+                else:
+                    datasheet["C{}".format(counter)] = "Not Approved"
+                datasheet["D{}".format(counter)] = participation.participant.email_address
+                datasheet["E{}".format(counter)] = participation.participant.phone
                 counter += 1
-                ws["A{}".format(counter)] = event.name
-                ws["B{}".format(counter)] = event.short_description
-                ws["C{}".format(counter)] = event.rules
-                ws["D{}".format(counter)] = event.category.name
-                ws["E{}".format(counter)] = event.contact
 
-            # return the document
-            response = HttpResponse(content=save_virtual_workbook(wb), content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
-            response['Content-Disposition'] = "attachment; filename={}".format("EventData.xlsx")
+            # STEP 4: prepare the response and return the document
+            response = HttpResponse(content=save_virtual_workbook(databook), content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+            response['Content-Disposition'] = "attachment; filename={}".format("{}Data.xlsx".format(username))
             return response
 
         except KeyError as missing_data:
