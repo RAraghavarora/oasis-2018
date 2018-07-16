83e58358126124b5ef071cee239d3985ebaf2e38
RAraghavarora <raghavarora999@yahoo.in>
2018-07-16 12:06:05 +0530

    New middleware added with a sendgrid script to send emails for unhandled exceptions

--- events/sg.py
+++ events/sg.py
@@ -6,7 +6,18 @@ from sendgrid.helpers.mail import *
 
 sg = sendgrid.SendGridAPIClient(apikey='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')
 from_email = Email("raghavarora2012.ra@gmail.com")
-email_list = ['f20171016@pilani.bits-pilani.ac.in', 'raghavarora2012.ra@gmail.com', 'raghavarora999@yahoo.in']
+email_list = [
+    'f20171016@pilani.bits-pilani.ac.in',
+    'f20170216@pilani.bits-pilani.ac.in', 
+    'f20170636@pilani.bits-pilani.ac.in',
+    'f20171196@pilani.bits-pilani.ac.in',
+    'f20171170@pilani.bits-pilani.ac.in',
+    'f2016023@pilani.bits-pilani.ac.in',
+    'f2016036@pilani.bits-pilani.ac.in',
+    'f2016153@pilani.bits-pilani.ac.in',
+    'f2015831@pilani.bits-pilani.ac.in',
+    'f2015129@pilani.bits-pilani.ac.in'
+    ]
 
 con = '''
 Exception occured in the Website!
@@ -21,7 +32,7 @@ def send_email():
         to_email = Email(i)
         mail = Mail(from_email, subject, to_email, content)
         response = sg.client.mail.send.post(request_body=mail.get())
-    print("MAIL = SENT")
+    #print("MAIL = SENT")
 
 #print(response.status_code)
 #print(response.body)
