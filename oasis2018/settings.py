d45be2373bea5d5d7e435a6fea271f8f9b3158f7
sanchit-ahuja <sanchitahuja205@gmail.com>
2018-07-23 20:20:48 +0530

    UTF problem changes in settings

--- oasis2018/settings.py
+++ oasis2018/settings.py
@@ -86,6 +86,7 @@ try:
             'PASSWORD': DATABASE_PASSWORD,
             'HOST': 'localhost',
             'PORT': '3306',
+            'OPTIONS': {'charset': 'utf8mb4'},
         }
     }
 except Exception as error_message:
