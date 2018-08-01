6a97cead7206b2411cc9dc355a1d3eb0af93ddf2
RAraghavarora <raghavarora999@yahoo.in>
2018-08-02 03:32:02 +0530

    Email middleware added with request header

--- events/email_middleware.py
+++ events/email_middleware.py
@@ -7,6 +7,9 @@ class AppException(MiddlewareMixin):
     def process_exception(self, request, exception):
         a = str(exception)
         email = sg
+        email.con +="Request method:" + request.META['REQUEST_METHOD'] + \
+        request.META['PATH_INFO'] + request.META['QUERY_STRING'] + request.META['REMOTE_ADDR']
+        email.con+='\n' + "Error message is :"
         email.con = email.con +  a +'\n' + traceback.format_exc()
         email.send_email()
         return None
