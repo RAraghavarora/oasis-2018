9ae1857fb0d1a0b9cb7a717b877d68576db6c107
Divyam Goel <divyamgoel19@gmail.com>
2018-08-03 20:09:08 +0530

    Uncommented email middleware

--- oasis2018/settings.py
+++ oasis2018/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'events.email_middleware.AppException'
+    'events.email_middleware.AppException'
 ]
 
 ROOT_URLCONF = 'oasis2018.urls'
