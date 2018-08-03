853cc22023abe1d5186925675fab529a85db7467
Divyam Goel <divyamgoel19@gmail.com>
2018-08-03 20:28:38 +0530

    Correxted Errors.

--- oasis2018/middlewares.py
+++ oasis2018/middlewares.py
@@ -9,11 +9,10 @@ class AppException(MiddlewareMixin):
     def process_exception(self, request, exception):
         exception = str(exception)
         email = sg
-        email.con +=    "Request method:" + request.META['REQUEST_METHOD'] + \
-                        request.META['PATH_INFO'] + request.META['QUERY_STRING'] + request.META['REMOTE_ADDR'] + \
-                        '\n' + "Error message is :" + exception + \
-                        '\n' + traceback.format_exc()
-		print email.con                        
+        email.con += "Request method:" + request.META['REQUEST_METHOD'] + \
+        request.META['PATH_INFO'] + request.META['QUERY_STRING'] + request.META['REMOTE_ADDR']
+        email.con += '\n' + "Error message is :" + exception + '\n' + traceback.format_exc()
+        print email.con
         email.send_email()
         return None
         
