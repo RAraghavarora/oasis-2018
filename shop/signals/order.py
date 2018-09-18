0f7dc1c3a6b1a4db9a149a3205fbb981477e8359
Hemanth V. Alluri <hdrive1999@gmail.com>
2018-09-18 19:54:10 +0530

    git rm -r --cached .

---
+++ shop/signals/order.py
@@ -0,0 +1,47 @@
+import json
+
+from django.conf import settings
+from django.dispatch import receiver
+from django.db.models.signals import post_save, pre_delete
+from rest_framework.renderers import JSONRenderer
+
+from firebase_admin import firestore
+
+from shop.models.order import Order, OrderFragment
+from shop.serializers import OrderSerializer, OrderFragmentSerializer
+
+"""
+@receiver(post_save, sender=Order)
+def orderFirebaseUpdate(sender, **kwargs):
+    db = firestore.client()
+    data = OrderSerializer(kwargs["instance"]).data
+    col_str = "User #{}".format(kwargs["instance"].customer.user.id)
+    collection = db.collection(col_str)
+    collection.document("Order #{}".format(kwargs["instance"].id)).set(data)
+
+
+@receiver(pre_delete, sender=Order)
+def orderFirebaseDelete(sender, **kwargs):
+    db = firestore.client()
+    col_str = "User #{}".format(kwargs["instance"].customer.user.id)
+    collection = db.collection(col_str)
+    collection.document("Order #{}".format(kwargs["instance"].id)).delete()
+"""
+
+@receiver(post_save, sender=OrderFragment)
+def orderFragmentFirebaseUpdate(sender, **kwargs):
+    db = firestore.client()
+    data = OrderFragmentSerializer(kwargs["instance"]).data
+    col_str = "User #{}".format(kwargs["instance"].stall.id)
+    collection = db.collection(col_str)
+    doc_string = "OrderFragment #{}".format(kwargs["instance"].id)
+    collection.document(doc_string).set(data)
+
+
+@receiver(pre_delete, sender=OrderFragment)
+def orderFragmentFirebaseDelete(sender, **kwargs):
+    db = firestore.client()
+    col_str = "User #{}".format(kwargs["instance"].stall.id)
+    collection = db.collection(col_str)
+    doc_string = "OrderFragment #{}".format(kwargs["instance"].id)
+    collection.document(doc_string).delete()
