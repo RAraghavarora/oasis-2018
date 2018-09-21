aad100e9b08df87d598da463aed858eebd7527a9
Sanchit <sanchit-ahuja@gitlab.com>
2018-09-21 17:09:53 +0530

    previous year templates added

--- regsoft/views.py
+++ regsoft/views.py
@@ -1,1 +1,33 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
+from registrations.models import *
+from .models import *
+from events.models import *
+from django.contrib.auth.models import User
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth import login, logout
+from django.core.urlresolvers import reverse
+from django.views.decorators.http import require_http_methods
+from django.db.models import Q
+from functools import reduce
+from registrations.urls import *
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+from oasis2018.settings import BASE_DIR
+import os
+from time import gmtime, strftime
+import string
+from pcradmin.views import get_cr_name, gen_barcode, get_pcr_number
+from django.contrib import messages
+from django.contrib.auth.models import User
+import sendgrid
+import os
+import re   
+from sendgrid.helpers.mail import *
+from oasis2017.keyconfig import *
+import string
+from random import sample, choice
+from django.contrib import messages 
+
+
