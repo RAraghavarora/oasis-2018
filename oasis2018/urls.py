9d73010822fb55c81c44768dca1cb816fef3824c
Hemanth V. Alluri <hdrive1999@gmail.com>
2018-09-21 22:37:53 +0530

    fixed urls issue as per Raghav's discretion

--- oasis2018/urls.py
+++ oasis2018/urls.py
@@ -22,12 +22,12 @@ from django.views.generic import RedirectView
 from django.views.static import serve
 
 urlpatterns = [
-    url(r'^2018/admin/', admin.site.urls),
-    url(r'^2018/preregistration/',include('preregistration.urls')),
-    url(r'^2018/events/',include('events.urls')),
-    url(r'^2018/registrations/',include('registrations.urls')),
-    url(r'^2018/analytics/', include('analytics.urls')),
-    url(r'^2018/pcradmin/', include('pcradmin.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^preregistration/',include('preregistration.urls')),
+    url(r'^events/',include('events.urls')),
+    url(r'^registrations/',include('registrations.urls')),
+    url(r'^analytics/', include('analytics.urls')),
+    url(r'^pcradmin/', include('pcradmin.urls')),
 ]
 
 
