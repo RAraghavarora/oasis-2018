1143b626b8526712b4dd0fbd90f726814fc89f8b
Sanchit <sanchit-ahuja@gitlab.com>
2018-09-21 18:26:21 +0530

    Some done

--- regsoft/views.py
+++ regsoft/views.py
@@ -31,3 +31,68 @@ from random import sample, choice
 from django.contrib import messages 
 
 
+#########Recnacc#########
+
+##Helper function to get group leader##
+def get_group_leader(group):
+    return group.participant_set.get(is_g_leader=True)
+
+@staff_member_required
+def recnacc_home(request):
+    rows=[{'data':[group.group_code,get_group_leader(group).name,get_group_leader(group).college.name,get_group_leader(group).phone,group.created_time,group.participant_set.filter(controlz=True).count(),group.participant_set.filter(checkout_group__isnull=False).count()], 'link':[{'url':request.build_absolute_uri(reverse('regsoft:allocate_participants', kwargs={'g_id':group.id})), 'title':'Allocate Participants'}]} for group in Group.objects.all().order_by('-created time')]
+    title='Groups that have passed Firewallz'
+    headings = ['Group Code', 'Group Leader', 'College', 'Gleader phone', 'Firewallz passed time', 'Total controls passed','Total alloted', 'Checkout','View Participants']
+
+    table={
+        'rows':rows,
+        'title':title,
+        'headings':headings
+    }
+    return render(request,'regsoft/tables.html',{'tables':[table,]})
+
+
+@staff_member_required
+def allocate_participants(request,g_id):
+    group=get_object_or_404(Group,id=g_id)
+    if request.method=='POST':
+        from datetime import *
+        data=request.POST
+        try:
+            group.amount_deduct=data['amount_retained']
+            group.save()
+        except:
+            pass
+        if data['action']=='allocate':
+            try:
+                parts_id=data.getlist('data')
+                room_id=data['room']
+                room=Room.objects.get(id=room_id)
+            except:
+                messages.warning(request,'Incomplete selection')
+                return redirect(request.META.get('HTTP_REFERER'))
+            if not parts_id:
+                messages.warning(request,'Incomplete selection')
+                return redirect(request.META.get('HTTP_REFERER'))
+            rows=[]
+            room.vacancy-=len(parts_id)
+            room.save()
+            for part_id in parts_id:
+                part=Participant.objects.get(id=part_id)
+                part.acco=True
+                part.room=room
+                part.recnacc_time=datetime.now()
+                part.save()
+        elif data['action']=='deallocate':
+            try:
+                parts_id = data.getlist('data')
+            except:
+                return redirect(request.META.get('HTTP_REFERER'))
+            for part_id in parts_id:
+                part = Participant.objects.get(id=part_id)
+                room = part.room
+                room.vacancy += 1
+                room.save()
+                part.acco = False
+                part.room = None
+                part.save()
+        return redirect()                
