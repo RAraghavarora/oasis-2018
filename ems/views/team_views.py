d929d48b20978d0eb905356820888b3b381420ba
Divyam Goel <divyamgoel19@gmail.com>
2018-09-27 19:15:44 +0530

    Added team views.

--- ems/views/team_views.py
+++ ems/views/team_views.py
@@ -7,25 +7,51 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
+from registrations.models import MainEvent
+from ems.models.team import Team
+
+from ems.serializers import TeamSerializer
+
 class TeamList(APIView):
 
 	permission_classes = (IsAuthenticated)
 
-	def get(self, request):
-		pass
+	def get(self, request, event_id):
+		try:
+			event = MainEvent.objects.get(id = event_id)
+		except Event.DoesNotExist:
+			msg = {"message" : "Event with the following id doesn't exist: {}".format(event_id)}
+			return Response(msg, status = status.HTTP_404_NOT_FOUND)
 
-	def post(self, request):
-		pass
+		teams = Team.objects.filter(event = event)
+		serializer = TeamSerializer(teams, many = True)
+
+		return Response(serializer.data, status = status.HTTP_200_OK)
+
+
+	def post(self, request, event_id, team_id):
+		try:
+			event = MainEvent.objects.get(id = event_id)
+		except Event.DoesNotExist:
+			msg = {"message" : "Event with the following id doesn't exist: {}".format(event_id)}
+			return Response(msg, status = status.HTTP_404_NOT_FOUND)
+
+		serializer = TeamSerializer(data = req)
 
 class TeamDetail(APIView):
 
 	permission_classes = (IsAuthenticated)
 
 	def get(self, request, team_id):
-		pass
+		try:
+			team = Team.objects.get(id = team_id)
+		except Team.DoesNotExist:
+			msg = {"mesage" : "Team doesn't exist"}
+			return Response(msg, status = status.HTTP_404_NOT_FOUND)
+
+		serializer = TeamSerializer(team)
+
+		return Response(serializer.data, status = status.HTTP_200_OK)
 
 	def post(self, request, team_id):
-		pass
-
-	def delete(self, request, team_id):
 		pass
