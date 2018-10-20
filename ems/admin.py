eb78e872f9d4f64da8339cfca34262dc2dd906cb
Divyam Goel <divyamgoel19@gmail.com>
2018-10-20 09:30:49 +0530

    Multiple changes in EMS

--- ems/admin.py
+++ ems/admin.py
@@ -1,1 +1,34 @@
 from django.contrib import admin
+
+from ems.models.clubdept import ClubDepartment
+from ems.models.judge import Judge
+from ems.models.level import LevelClass, LevelInstance
+from ems.models.parameter import ParameterClass, ParameterInstance 
+from ems.models.team import Team
+
+
+class LevelInstanceInLine(admin.TabularInline):
+    model = LevelInstance
+    extra = 1
+
+
+class LevelClassAdmin(admin.ModelAdmin):
+    inlines = [LevelInstanceInLine]
+
+
+class ParameterInstanceInLine(admin.TabularInline):
+    model = ParameterInstance
+    extra = 1
+
+
+class ParameterClassAdmin(admin.ModelAdmin):
+    inlines = [ParameterInstanceInLine]
+
+
+admin.site.register(ClubDepartment)
+admin.site.register(Team)
+admin.site.register(Judge)
+admin.site.register(LevelClass, LevelClassAdmin)
+admin.site.register(LevelInstance)
+admin.site.register(ParameterClass, ParameterClassAdmin)
+admin.site.register(ParameterInstance)
--- ems/models/judge.py
+++ ems/models/judge.py
@@ -6,11 +6,19 @@ from django.db import models
 from events.models import MainEvent
 
 class Judge(models.Model):
-	user = models.ForeignKey(settings.AUTH_USER_MODEL)
+	'''
+	Development Notes:
+	Does not have level field.
+	'''
+	name = models.CharField(max_length = 200)
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+	
 	event = models.ForeignKey(MainEvent)
+	
 	is_active = models.BooleanField(default=True)
 	frozen = models.BooleanField(default=False)
-	#parameterinstances : ParameterInstance
+	
+	#parameter_instances : ParameterInstance
 
 	def __str__(self):
 		return self.user.username
