20be36a875da01f651c38dfe8ad78355ae5599b9
Raghav Arora <raghavarora999@yahoo.in>
2018-10-21 22:05:42 +0530

    asd

--- ems/views/auth_views.py
+++ ems/views/auth_views.py
@@ -1,22 +1,26 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.shortcuts import render,redirect
 
-from rest_framework.views import APIView
 from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import FormParser, JSONParser, MultiPartParser
 
 class Login(APIView):
 
-	#renderer_classes = (TemplateHTMLRenderer,)
+	renderer_classes = (TemplateHTMLRenderer,)
+	parser_classes = (FormParser,JSONParser, MultiPartParser)
 
 	def post(self, request):
-
+		
 		try:
-			username = request.POST['username']
-			password = request.POST['password']
+			username = request.data['username']
+			password = request.data['password']
 		except KeyError as missing:
 			msg = 'The following field was missing: {}'.format(missing)
 			messages.warning(request, msg)
-		return redirect()
+			return redirect(request.META.get('HTTP_REFERER'))
 
 		user = authenticate(username=username, password=password)
 
@@ -25,12 +29,19 @@ class Login(APIView):
 			return redirect(request.META.get('HTTP_REFERER'))
 
 		login(request, user)
+
 		return redirect('ems:index')
+
+	def get(self, request):
+
+		return Response(template_name='ems/login.html')
 
 
 class Logout(APIView):
 
+	renderer_classes = (TemplateHTMLRenderer,)
+
 	def post(self, request):
 
 		logout(request)
-		return redirect('ems:index')
+		return redirect('ems:login')
--- ems/views/team_views.py
+++ ems/views/team_views.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
 
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.renderers import TemplateHTMLRenderer,JSONRenderer
 from rest_framework.views import APIView
+from rest_framework.parsers import FormParser, JSONParser, MultiPartParser
+
 
 
 from registrations.models import MainEvent
@@ -15,17 +18,22 @@ from ems.serializers import TeamSerializer
 
 class TeamList(APIView):
 
-	#permission_classes = (IsAuthenticated)
+	# permission_classes = (IsAuthenticated)
+	renderer_classes = (TemplateHTMLRenderer,JSONRenderer)
+	parser_classes = (FormParser,JSONParser, MultiPartParser)
 
-	renderer_classes = (TemplateHTMLRenderer,)
 
 	def get(self, request, event_id):
 		try:
 			event = MainEvent.objects.get(id = event_id)
-		except Event.DoesNotExist:
-			msg = {"message" : "Event with the following id doesn't exist: {}".format(event_id)}
-			return Response(msg, status = status.HTTP_404_NOT_FOUND)
-
+		except MainEvent.DoesNotExist:
+			context = {
+				'error_heading': "Error",
+				'message': "Event does not exist",
+				'url':request.build_absolute_uri(reverse('ems:index'))
+				}
+			return Response(context, template_name='registrations/message.html')
+		print(event)
 		teams = Team.objects.filter(event = event)
 		#serializer = TeamSerializer(teams, many = True)
 
@@ -37,18 +45,22 @@ class TeamList(APIView):
 		return Response(response, template_name = 'ems/team_details_home.html', status = status.HTTP_200_OK)
 
 
-	def post(self, request, event_id, team_id):
+	def post(self, request, event_id):
 		try:
 			event = MainEvent.objects.get(id = event_id)
 		except Event.DoesNotExist:
 			msg = {"message" : "Event with the following id doesn't exist: {}".format(event_id)}
 			return Response(msg, status = status.HTTP_404_NOT_FOUND)
 
+		data = request.data
+		if data['submit']=='delete_teams':
+			print('yes')
+
 		serializer = TeamSerializer(data = req)
 
 class TeamDetail(APIView):
 
-	permission_classes = (IsAuthenticated)
+	# permission_classes = (IsAuthenticated)
 
 	def get(self, request, team_id):
 		try:
