ded6cb05400120f03d4bae7965b890c6277510bb
Divyam Goel <divyamgoel19@gmail.com>
2018-10-22 16:01:44 +0530

    Removed a lot of print statements.

--- registrations/views/chor.py
+++ registrations/views/chor.py
@@ -22,7 +22,6 @@ def register(request):
 		return render(request,'registrations/chor_register.html',context)
 	elif request.method=='POST':
 		data = request.POST
-		# print(data)
 		if len(data['phone'])!=10:
 			return JsonResponse({'status':0,'message':'Please enter a valid contact number.'})
 		try:
@@ -62,7 +61,6 @@ def register(request):
 		    return response
 		participant.is_chor=True
 		participant.save()
-		print(data.getlist('events[]'))
 
 		for key in data.getlist('events[]'):
 			event = MainEvent.objects.get(id=int(key))
@@ -75,19 +73,15 @@ def register(request):
 		to_email = Email(send_to)
 		verify_email_url = str(request.build_absolute_uri(reverse("registrations:index"))) + 'email_confirm/' + \
 		generate_email_token(Participant.objects.get(email=send_to)) + '/'
-		print('Email url \t',verify_email_url)
 		mail.body = mail.body%(name.title(), verify_email_url)
 		content = Content('text/html', mail.body)
 		# 
-		# print(data['events'][0])
 		try:
 			mail_1 = Mail(mail.from_email, mail.subject, to_email, content)
 			response = send_grid.sg.client.mail.send.post(request_body = mail_1.get())
 			if response.status_code%100!=2:
 				raise Exception
-			print(response)
 		except Exception as e:
-		    print("\t",e)
 		    participant.delete()
 		    return JsonResponse({'status':0, 'message':'Error sending email. Please try again.'})
 
