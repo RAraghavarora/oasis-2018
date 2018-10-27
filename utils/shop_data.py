7f22e5b107389bc273cd181dc19b302f16e1bbed
sanchit <sanchitahuja205@gmail.com>
2018-10-27 13:21:51 +0530

    stall script done

--- utils/shop_data.py
+++ utils/shop_data.py
@@ -4,12 +4,12 @@ from django.contrib.auth.models import User
 from oasis2018.settings import BASE_DIR
 
 
-with open(BASE_DIR + '/utils/stalls_menu2.csv') as cfile:
+with open(BASE_DIR + '/utils/STALLS-MENU.csv') as cfile:
     reader = csv.reader(cfile, delimiter=',')
     for row in reader:
         try:
-            stall = Stall.objects.get(name=row[-1])
+            stall = Stall.objects.get(name=row[0])
         except Stall.DoesNotExist:
-            user, created = User.objects.get_or_create(username=row[-1])
-            stall = Stall.objects.create(user=user, name=row[-1])
-    ItemClass.objects.create(name=row[0], stall=stall, price=row[1], )
+            user, created = User.objects.get_or_create(username=row[0])
+            stall = Stall.objects.create(user=user, name=row[0])
+        ItemClass.objects.create(name=row[1], stall=stall, price=int(row[2]), )
--- utils/shop_data_excel.py
+++ utils/shop_data_excel.py
@@ -1,14 +1,21 @@
 from openpyxl import *
 from shop.models import *
+from oasis2018.settings import BASE_DIR
+from registrations.models import Bitsian
+from django.contrib.auth.models import User
 
 filename = BASE_DIR + "/utils/student_list2.xlsx"
 filename2='/home/sanchit/Downloads/STALLS MENU.xlsx'
-wb = load_workbook(filename=filename)
-sheet = wb["Sheet1_2"]
+wb = load_workbook(filename=filename2)
+sheet = wb["Sheet1"]
 length = len(tuple(sheet.rows))
-
+stall_list=[]
 for i in range(2,length):
     stall_name=sheet.cell(row=i,column=1).value
     item_name=sheet.cell(row=i,column=2).value
     price=sheet.cell(row=i,column=3).value
+    stall_list.append(stall_name)
+    
+    # user, created = User.objects.get_or_create(username=row[-1])
+
     
