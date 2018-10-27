3d9e2d8c12801e35e6d12fb4e483ae82db1c074c
Nayan Khanna <f20170636@pilani.bits-pilani.ac.in>
2018-10-27 12:29:40 +0530

    change

--- utils/wallet_script.py
+++ utils/wallet_script.py
@@ -1,6 +1,7 @@
 from registrations.models import *
 from events.models import *
 from shop.models.wallet import Wallet
+from shop.models.balance import Balance
 
 for i in Group.objects.all():
     for x in i.participant_set.all():
