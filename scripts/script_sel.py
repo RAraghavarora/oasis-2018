52b146d210228116f001f65a3f0901aaabdc6c38
sanchit <sanchitahuja205@gmail.com>
2018-10-29 10:12:14 +0530

    SEL script

---
+++ scripts/script_sel.py
@@ -0,0 +1,50 @@
+from registrations.models import *
+from shop.models.item import Tickets
+import sys
+import csv
+from shop.models import *
+# from openpyxl import *
+# filename2='/home/sanchit/DVM_SHIT/oasis-2018/scripts/SEL and EDM signings sheet Pre-Oasis.xlsx'
+# f1 = '/home/sanchit/DVM_SHIT/oasis-2018/scripts/FINAL STUDENT LIST.xlsx'
+# wb = load_workbook(filename=filename2)
+# sheet = wb["Sheet1"]
+# wb2 = load_workbook(filename=f1)
+# sheet2 = wb2['MASTERSHEET'] #Student list
+# length = len(tuple(sheet.rows))
+
+p1 = MainProfShow.objects.get(name__icontains="shankar")
+p2 = MainProfShow.objects.get(name__icontains="edm")
+
+with open("scripts/eggs.csv") as f:
+    wb = csv.reader(f)
+    for row in wb:
+        email = row[0]
+        a = int(row[1])
+        b = int(row[2])
+        c = int(row[3])
+        a+=c
+        b+=c
+        try:
+            bit = Bitsian.objects.get(email=email)
+        except:
+            continue
+        try:
+            user = bit.user
+        except:
+            continue
+        if a>0:
+            try:
+                t = Tickets.objects.get(prof_show=p1, user=user)
+                t.count += a
+                t.save()
+            except:
+                t =  Tickets.objects.create(prof_show=p1, user=user, count = a)
+        if b>0:
+            try:
+                t = Tickets.objects.get(prof_show=p2, user=user)
+                t.count += b
+                t.save()
+            except:
+                t =  Tickets.objects.create(prof_show=p2, user=user, count = b)
+        
+        
--- scripts/sel_generate.py
+++ scripts/sel_generate.py
@@ -1,7 +1,7 @@
 import sys
 from openpyxl import *
 filename2='/home/sanchit/DVM_SHIT/oasis-2018/scripts/SEL and EDM signings sheet Pre-Oasis.xlsx'
-f1 = '/home/sanchit/DVM_SHIT/oasis-2018/scripts/student_list3.xlsx'
+f1 = '/home/sanchit/DVM_SHIT/oasis-2018/scripts/FINAL STUDENT LIST.xlsx'
 wb = load_workbook(filename=filename2)
 sheet = wb["Sheet1"]
 wb2 = load_workbook(filename=f1)
@@ -9,29 +9,38 @@ sheet2 = wb2['MASTERSHEET'] #Student list
 length = len(tuple(sheet.rows))
 emailist=[]
 long_list=[]
-idd_list=[]
 l=[]
 l2 = len(tuple(sheet2.rows))
 p = []
 for j in range(2, l2):
     idd = sheet2.cell(row=j, column=2).value
-    ee = sheet2.cell(row=j, column=10).value
+    ee = sheet2.cell(row=j, column=8).value
     p.append([ee, idd])
-    idd_list.append(idd)
 dick = {idd: ee for ee, idd in p}
 #print(len(dick))
-
-for i in range(2, length):
+fin = []
+lit = 0
+for i in range(2, length+1):
 
     un= str(sheet.cell(row=i,column=1).value)
     long_id = sheet.cell(row=i, column=1).value
-    long_list.append(long_id)
     try:
         l.append(dick[long_id])
-        
-    except:
+        email = dick[long_id]
+        # print(1)
+        fin.append([email, sheet.cell(row=i, column=8).value, sheet.cell(row=i, column=9).value, sheet.cell(row=i, column=10).value])
+        lit+=2
+    except Exception as e:
+        print (3)
+        print(i, e)
         pass
+import csv
+with open('eggs.csv', 'w') as f:
+    w = csv.writer(f)
+    for i in fin:
+        w.writerow(i)
+print(len(long_list))
 
-print(len(l))
+
 
 
